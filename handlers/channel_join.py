f8c4546e35a70c9f9188f7afb4047f3288d7a9b3
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2026-02-07 23:56:48 +0500

    add .py

---
+++ handlers/channel_join.py
@@ -0,0 +1,41 @@
+from aiogram.types import ChatMemberUpdated
+from loader import router_channel
+from database.db_users import DB
+import aiosqlite
+from datetime import datetime
+
+@router_channel.my_chat_member()
+async def bot_added_to_channel(event: ChatMemberUpdated):
+    if event.chat.type != "channel":
+        return
+
+    if event.new_chat_member.status in ("administrator", "creator"):
+        async with aiosqlite.connect(DB) as db:
+
+            # USER
+            if event.from_user:
+                await db.execute("""
+                    INSERT OR IGNORE INTO users
+                    (user_id, username, full_name, joined_at)
+                    VALUES (?, ?, ?, ?)
+                """, (
+                    event.from_user.id,
+                    event.from_user.username,
+                    event.from_user.full_name,
+                    datetime.now().isoformat()
+                ))
+
+            # CHANNEL
+            await db.execute("""
+                INSERT OR IGNORE INTO channels
+                (channel_id, channel_title, channel_username, owner_id, added_at)
+                VALUES (?, ?, ?, ?, ?)
+            """, (
+                event.chat.id,
+                event.chat.title,
+                event.chat.username,
+                event.from_user.id if event.from_user else None,
+                datetime.now().isoformat()
+            ))
+
+            await db.commit()
